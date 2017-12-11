4385b1420c3fea5931fe1aab65b0e1156343c7f7
Bootcamp <njackson422@gmail.com>
2017-12-10 22:21:30 -0500

    corrects file structure

--- controllers/task_controller.js
+++
@@ -1,7 +0,0 @@
-//pseudocode
-
-/*on login find out if parent or child 
-get some indication if parent or child 
-use the information to tell if parent or child to go to different partials 
-partial for parent and partial for child 
-in index.handlebars would have an if stmnt that says if parent...use parent partial and if child...use child partial*/
