56bdec3ab79bfc67e139fc4f1282c0e26d2cdb70
Bootcamp <njackson422@gmail.com>
2017-12-12 09:36:22 -0500

    adds pseudocoding for routing

---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+console.log("API Routes working");
+
+module.exports= function(app){
+
+	// post route for parentn to create new task
+		//what info will they pass in (name/description/etc)
+		//upon submit generate token
+
+	//get route where parent can view all tasks and completed yes/no
+		//possibly two different routes for completed or not
+		//bonus: bonus checklists if completed by due date
+
+	// get(?) route to fire html link to enter magic link
+
+	//get route for html that child sees upon login
+
+	//post route to handles adding gifts to wishlist
+
+	//bonus: parent has delete and edit routes for tasks,
+	//bonus: child has delete and edit routes for wishlist gifts 
+
+};
+
+
+
---
+++ routes/html-routes.js
@@ -0,0 +1,5 @@
+console.log("HTML Routes working");
+
+module.exports= function(app){
+
+};
