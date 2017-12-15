d69294b4c3d2e801d1ccf5af7e50610c8a581024
Bootcamp <njackson422@gmail.com>
2017-12-14 21:21:36 -0500

    update routing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,6 @@
-console.log("API Routes working");
-
 var db = require("../models");
 
 module.exports= function(app){
-	//Test connection
-	app.get('/', function (req, res) {
-  		res.send('hello world')
-	})
 
 	// post route for parent to create new task
 	// 	what info will they pass in (name/description/etc)
@@ -16,30 +10,49 @@ module.exports= function(app){
 	      task_name: req.body.task_name,
     }).then(function(dbPost) {
     	console.log("/parents routing working");
-    	// res.json(dbPost);
+    	res.json(result);
     });
   });
-
 
 
 //get route where parent can view all tasks and completed yes/no
 	//possibly two different routes for completed or not
 	//bonus: bonus checklists if completed by due date
-
-  // app.get("/parents", function(req, res) {
-  //   db.Tasks.findAll({
-  //   	//tasks
-  //   }).then(function(santasHelp_db) {
-  //     res.json(result);
-  //   });
-  // });
+	app.get("/parents", function(req, res) {
+	    db.Tasks.findAll({
+	    	include: [db.Tasks]
+    	}).then(function(santasHelp_db) {
+      res.json(result);
+    });
+  });
 
 
 	// get(?) route to fire html link to enter magic link
+	// app.get("/", function(req, res) {
+	//     db.Tasks.findAll({
+	//     	include: [db.Tasks]
+ //    	}).then(function(santasHelp_db) {
+ //      res.json(result);
+ //    });
+ //  });
 
 	//get route for html that child sees upon login
+	app.get("/:id", function(req, res) {
+	    db.Tasks.findAll({
+	    	include: [db.Tasks]
+    	}).then(function(santasHelp_db) {
+      res.json(result);
+    });
+  });
 
 	//post route to handles adding gifts to wishlist
+	app.post("/createWishList", function(req, res) {
+	    db.Gifts.create({
+	      Gift: req.body.gift_name,
+    }).then(function(dbPost) {
+    	res.json(result);
+    });
+  });
 
 	//bonus: parent has delete and edit routes for tasks,
 	//bonus: child has delete and edit routes for wishlist gifts 
