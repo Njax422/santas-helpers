e70bc8e51cd18412be34d6c1a9a0231bf3d5fa66
Bootcamp <njackson422@gmail.com>
2017-12-14 21:11:08 -0500

    add all

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,1 @@
-CREATE DATABASE santasHelp_db;
-
-USE santasHelp_db;
-
-CREATE TABLE parents (
-  id INT NOT NULL AUTO_INCREMENT,
-  fname VARCHAR(256) NOT NULL,
-  lname VARCHAR(256) NOT NULL,
-  email VARCHAR(256) NOT NULL,
-  date timeStamp,
-  PRIMARY KEY(id)
-);
+CREATE DATABASE santashelp_db;
