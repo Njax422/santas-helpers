06eb17f41a6f95bea0f19aa41fd31792eb2c7795
Michelle <manneocampo@gmail.com>
2017-12-16 00:28:20 -0500

    finished passport code and added passport-http-bearer authentication

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -4,7 +4,7 @@ var bCrypt = require('bcrypt-nodejs');
 module.exports = function(passport, user){
   var User = user;
   var LocalStrategy = require('passport-local').Strategy;
-
+  var BearerStrategy = require('passport-http-bearer').Strategy;
 passport.use('local-signup', new LocalStrategy (
   {
     usernameField: 'email',
@@ -63,5 +63,56 @@ passport.use('local-signup', new LocalStrategy (
       }
     });
   });
+  //local signin
+  passport.use('local-signin', new LocalStrategy (
+    {
+      //by default, local strategy uses username and password, we will override w email
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+
+    function(req, email, password, done) {
+      var User = user;
+
+      var isValidPassword = function(userpass, password) {
+        return bCrypt.compareSync(password, userpass);
+      }
+      User.findOne({
+        where: {
+          email: email
+        }
+      }).then(function(user){
+        if (!user) {
+          return done (null, false, {
+            message: 'Email does not exist'
+          });
+        }
+        if (!isValidPassword(user.password, password)) {
+          return done(null, false, {
+            message: 'Incorrect password.'
+          });
+        }
+        var userinfo = user.get();
+        return done(null, userinfo);
+      }).catch(function(err){
+        console.log("Error:", err);
+
+        return done(null, false, {
+          message: 'Something jwent wrong with your Signin'
+        });
+      });
+    }
+  ));
+
+  passport.use(new BearerStrategy (
+    function(token, done) {
+      User.findOne({ token: token }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        return done(null, user, {scope: 'all'});
+      });
+    }
+  ));
 
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 var authController = require('../controllers/authcontroller.js');
+
 module.exports = function(app, passport){
+
     app.get('/signup', authController.signup);
 
     app.get('/signin', authController.signin);
@@ -10,13 +12,28 @@ module.exports = function(app, passport){
       failureRedirect: '/signup'
       }
     ));
+
     app.get('/dashboard',isLoggedIn, authController.dashboard);
 
     app.get('/logout', authController.logout);
+
+    app.post('/signin', passport.authenticate('local-signin', {
+      successRedirect: '/dashboard',
+
+      failureRedirect: '/signin'
+      }
+    ));
 
     function isLoggedIn(req, res, next) {
       if (req.isAuthenticated())
         return next();
       res.redirect('/signin');
     }
+
+    app.get('/profile',
+    passport.authenticate('bearer', { session: false }),
+    function(req, res) {
+      res.json(req.user);
+    });
+
 }
