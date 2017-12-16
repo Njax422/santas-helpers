4c7d5e5cadcc563cd7e0c3386458f343407743af
Michelle <manneocampo@gmail.com>
2017-12-16 10:23:31 -0500

    commented out var server in server js for oauth

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var env = require('dotenv').load();
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-var server = oauth2orize.createServer();
+// var server = oauth2orize.createServer();
 
 //For BodyParser
 app.use(bodyParser.json());
