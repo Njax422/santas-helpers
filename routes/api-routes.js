7985784b6328773474c2e8228e5f40ff03f6a24b
Bootcamp <njackson422@gmail.com>
2017-12-19 02:13:55 -0500

    continues testing displaying to dashboard with routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,34 +2,32 @@ var db = require("../models");
 module.exports= function(app){
 
 	//Populates data to the parent dashboard upon logging in, based on userId
-	app.get("/dashboard", function(req, res) {
-			var data = {};
-			db.Task.findAll({
-				where: {
-	    		//********Once we have data in the db, replace with variable of user ID********
-    			userId: 1
-  			}
-			}).then(function(tasks) {
-				data.task=tasks;
-			db.Gift.findAll({
-				where: {
-	    		//********Once we have data in the db, replace with variable of user ID********
-    			userId: 1
-  			}
-			}).then(function(gifts) {
-				data.gift=gifts;
-			res.render('dashboard.handlebars', data);
-				// console.log(data);
-			})
-   		 });
- 	 });
+	// app.get("/dashboard", function(req, res) {
+	// 		var data = {};
+	// 		db.Task.findAll({
+	// 			where: {
+ //    			userId: 1 //replace with variable of user ID
+ //  			}
+	// 		}).then(function(tasks) {
+	// 			data.task=tasks;
+	// 		db.Gift.findAll({
+	// 			where: {
+ //    			userId: 1 //replace with variable of user ID
+ //  			}
+	// 		}).then(function(gifts) {
+	// 			data.gift=gifts;
+	// 		res.render('dashboard.handlebars', data);
+	// 			// console.log(data);
+	// 		})
+ //   		 });
+ // 	 });
 
 
 	// Creates new task and redirected to /tasklist for updating list
 	app.post("/child", function(req, res) {
 			db.Task.create({
 	      task: req.body.task,
-	      userId: 1
+	      userId: 1 //replace with variable of user ID
     }).then(function(result) {
     	res.redirect("/tasklist");
     	});
@@ -41,15 +39,13 @@ module.exports= function(app){
 			var data = {};
 			db.Task.findAll({
 				where: {
-	    		//********Once we have data in the db, replace with variable of user ID********
-    			userId: 1
+    			userId: 1 //replace with variable of user ID
   			}
 			}).then(function(tasks) {
 				data.task=tasks;
 			db.Gift.findAll({
 				where: {
-	    		//********Once we have data in the db, replace with variable of user ID********
-    			userId: 1
+    			userId: 1 //replace with variable of user ID
   			}
 			}).then(function(gifts) {
 				data.gift=gifts;
@@ -62,12 +58,12 @@ module.exports= function(app){
 
 	//Displays task to childs to do list
 	//Changes we made during tutor session change the functionality of submit so it now longer goes to child page, will come back and modify this later
-    app.post("/child", function(req, res) {
-            db.Task.create({
+    app.post("/child", function(req, res){
+        db.Task.create({
           task: req.body.task,
+          userId: 1
     }).then(function(result) {
-        console.log("Childs tasks:", result);
-      res.render('child.handlebars', {task: result.dataValues.task});
+      res.render('child.handlebars', {task: result.dataValues});
     });
   });
 
@@ -80,17 +76,5 @@ module.exports= function(app){
   	// res.render('dashboard.handlebars', {task: result});
     	});
   });
-
-	//get route for html that child sees upon login
-	// app.get("/:id", function(req, res) {
-	//     db.Tasks.findAll({
-	//     	include: [db.Tasks]
-  //   	}).then(function(santasHelp_db) {
-  //     res.json(result);
-  //   });
-  // });
-	//Michelle commented this section^ out 12/15 to
-	//keep moving forward with passport, was creating error
-	//cannot get findAll of undefined
 
 };
